ad7612b4bfe6536d0d1243e1a4915c6fa912d311
Adrien RAMOS <ar081997@iut21.u-bourgogne.fr>
2022-03-18 13:58:23 +0100

    Fix convertisseur fichier + retrait nom id

--- projet/src/graphe/Graphe.java
+++ projet/src/graphe/Graphe.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class Graphe {
-    private boolean orientation;
-    private Map<String,Map<String,Integer>> listeAdjacence;
+    private final boolean orientation;
+    private final Map<Integer,Map<Integer,Integer>> listeAdjacence;
 
     public Graphe(boolean orientation){
         this.orientation = orientation;
@@ -16,13 +16,13 @@ public class Graphe {
         this(false);
     }
 
-    public void ajoutSommet(String sommet) {
+    public void ajoutSommet(int sommet) {
         if (!this.listeAdjacence.containsKey(sommet)) {
             this.listeAdjacence.put(sommet,new HashMap<>());
         }
     }
 
-    public void ajoutArete(String sommetSource, String sommetDestination, int poid) {
+    public void ajoutArete(int sommetSource, int sommetDestination, int poid) {
         this.ajoutSommet(sommetSource);
         this.ajoutSommet(sommetDestination);
         this.listeAdjacence.get(sommetSource).put(sommetDestination,poid);
@@ -31,7 +31,7 @@ public class Graphe {
         }
     }
 
-    public int getPoids(String sommetSource,String sommetDestination) {
+    public int getPoids(int sommetSource,int sommetDestination) {
         if (this.listeAdjacence.containsKey(sommetSource) && this.listeAdjacence.get(sommetSource).containsKey(sommetDestination)) {
             return this.listeAdjacence.get(sommetSource).get(sommetDestination);
         } else {
@@ -39,11 +39,11 @@ public class Graphe {
         }
     }
 
-    public Set<String> getSommets() {
+    public Set<Integer> getSommets() {
         return this.listeAdjacence.keySet();
     }
 
-    public Set<String> getVoisins(String sommet) {
+    public Set<Integer> getVoisins(int sommet) {
         if (this.listeAdjacence.containsKey(sommet)) {
             return this.listeAdjacence.get(sommet).keySet();
         } else {
@@ -53,39 +53,26 @@ public class Graphe {
 
     public void exportGraph() {
         int id = 0;
-        Map<Integer,String> identifiant= new HashMap<>();
-
-        for (String sommetCourant : this.listeAdjacence.keySet()) { // Initialiser la table d'identifiants
-            identifiant.put(id,sommetCourant);
-            id+= 1;
-        }
 
         try {
             File fichier = new File("Fichier/test.txt");
             String text = "";
-            for ( int idSommet : identifiant.keySet()) {
-                String sommetCourant = identifiant.get(idSommet);
-                text += idSommet + "=" + "" + sommetCourant + ";";
-            }
-            text += "\n";
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fichier));
+
             text += this.listeAdjacence.size() + "\n";
-            for (int idCourant : identifiant.keySet()) {
-                String sommetCourant = identifiant.get(idCourant);
-                for (int idVoisin : identifiant.keySet()) {
-                    String sommetVoisin = identifiant.get(idVoisin);
+            for (int idCourant = 0;idCourant<this.listeAdjacence.size();idCourant++) {
+                for (int idVoisin = 0;idVoisin<this.listeAdjacence.size();idVoisin++) {
                     if (idVoisin == idCourant) {
                         text += "0 ";
-                    } else if (this.listeAdjacence.get(sommetCourant).containsKey(sommetVoisin)) {
-                        text += this.listeAdjacence.get(sommetCourant).get(sommetVoisin) + " ";
+                    } else if (this.listeAdjacence.get(idCourant).containsKey(idVoisin)) {
+                        text += this.listeAdjacence.get(idCourant).get(idVoisin) + " ";
                     } else {
                         text += "x ";
                     }
                 }
-                text += "\n";
             }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fichier));
             writer.write(text);
-            writer.close(); //Il faut absolument fermer le writer après utilisation
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -97,21 +84,13 @@ public class Graphe {
         try {
             File fichier = new File("Fichier/test.txt");
             BufferedReader reader = new BufferedReader(new FileReader(fichier));
-            String tableIdentifiants = reader.readLine();
-            String[] identifieurs = tableIdentifiants.split(";");
-            for ( String identifieur : identifieurs) {
-                String[] elements = identifieur.split("=");
-                identifiant.put(Integer.parseInt(elements[0]),elements[1]);
-            }
             int nombreSommet = Integer.parseInt(reader.readLine());
-            for (int i = 0;i<8;i++) {
+            for (int idCourant = 0;idCourant<8;idCourant++) {
                 String ligne = reader.readLine();
                 String[] voisins =  ligne.split(" ");
-                int idVoisin = 0;
-                for (String poid : voisins ) {
-                    if(!poid.equals("x")) {
-                        this.ajoutArete(identifiant.get(i),identifiant.get(idVoisin),Integer.parseInt(poid));
-                        idVoisin+=1;
+                for (int idVoisin = 0;idVoisin<nombreSommet;idVoisin++) {
+                    if (!voisins[idVoisin].equals("x")) {
+                        this.ajoutArete(idCourant, idVoisin, Integer.parseInt(voisins[idVoisin]));
                     }
                 }
             }
@@ -129,15 +108,9 @@ public class Graphe {
             dir = "-->";
         } else { result = "strict graph {\n"; dir="--"; }
         int id = 0;
-        Map<String,Integer> identifiant= new HashMap<>();
-        for (String sommetCourant : this.listeAdjacence.keySet()) {
-            identifiant.put(sommetCourant,id);
-            result+=id+"[label="+sommetCourant+"]\n";
-            id+= 1;
-        }
-        for (String sommetCourant : this.listeAdjacence.keySet()) {
-            for (String sommetVoisin : this.getVoisins(sommetCourant)) {
-                result += identifiant.get(sommetCourant) + " " + dir + " " + identifiant.get(sommetVoisin) + " [label=\"" + this.getPoids(sommetCourant,sommetVoisin) + "\"]\n";
+        for (int sommetCourant : this.listeAdjacence.keySet()) {
+            for (int sommetVoisin : this.getVoisins(sommetCourant)) {
+                result += sommetCourant + " " + dir + " " + sommetVoisin + " [label=\"" + this.getPoids(sommetCourant,sommetVoisin) + "\"]\n";
             }
         }
         result+="}";
